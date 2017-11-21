2468d8f96edaf0a8f20376b1157a2b98695275e2
Onisoroma <nicolasfernandez93@gmail.com>
2017-11-20 23:33:00 -0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -59,6 +60,30 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(404).send({errorMessage: 'Todo not found'});                    
         }
         res.status(200).send({todo});        
+    }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var _id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(_id)){
+        return res.status(400).send({errorMessage: 'This is an invalid ID'});
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(_id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
     }).catch((e) => res.status(400).send());
 });
 
