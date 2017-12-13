1a1b44837ff37800969e27954f06ed702aa41292
Nicolas Fernandez Amorosino <>
2017-12-13 17:23:45 -0300

    check if its running on travis

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 var env = process.env.NODE_ENV || 'development'
 
-console.log(process.env)
+console.log(process.env.TRAVIS)
 
-if (env === 'development' || env === 'test') {
+if ((env === 'development' || env === 'test') && !process.env.TRAVIS) {
   var config = require('./config.json')
   var envConfig = config[env]
 
