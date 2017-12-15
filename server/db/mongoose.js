602d9dca3b1db69776c987b53395f37e3809240f
Nicolas Fernandez Amorosino <>
2017-12-15 11:22:05 -0300

    Ignored npm-debug.log, added handling to the database connection method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,12 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
+}).then(() => {
+  console.log('The connection to the DB has been established')
+}).catch((e) => {
+  console.log(`The connection to the DB couldn't be established`)
+})
 
 module.exports = {mongoose}
