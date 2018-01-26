c74e756d071a242fd49ffa89fa44c8a1c210283d
Nicolás Alejandro Fernández Amorosino <nicolasfernandez93@gmail.com>
2018-01-26 20:52:50 -0300

    Added spending model

---
+++ server/models/spending.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+var Spending = mongoose.model('Spending', {
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    minlength: 1,
+    trim: true,
+    required: true
+  },
+  month: {
+    type: String,
+    require
+  },
+  createdAt: {
+    type: Number,
+    required: true
+  },
+  direct: {
+    type: Boolean,
+    default: false
+  }
+})
+
+module.exports = { Spending }
